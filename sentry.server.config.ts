29c847909ac30ca03720454ee436f37e71d9acd1
dandiagusatwork <DandiAgusAtWork@gmail.com>
2024-10-17 01:33:11 +0700

    3

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408", // DSN langsung ditulis di sini
+  dsn: "https://34fe9ea3bac5bcf1fd880ce74c76f259@o4508133810700288.ingest.us.sentry.io/4508133849300993", // DSN langsung ditulis di sini
 
   tracesSampleRate: 1, // Sesuaikan ini di produksi
   debug: false, // Ubah ke true untuk debugging saat pengaturan
