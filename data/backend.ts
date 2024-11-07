6b745f703fbcd0003fa0ba37f49844aa973e5d8e
dandiagusatwork <DandiAgusAtWork@gmail.com>
2024-11-07 21:54:55 +0700

    add: data backend

--- data/backend.ts
+++ data/backend.ts
@@ -1,25 +1,35 @@
 export const projects = [
   {
+    id: 2,
+    title: "Api Marketplace ",
+    des: "API for Marketplace that connects merchants and customers with product creation, purchasing, and discount features.",
+    img: "/Api-marketplace.png",
+    iconLists: ["/mysql.jpeg", "/node.png"],
+    link: "https://github.com/dawijaya/backend-dandi-agus-wijaya",
+  },
+  {
     id: 1,
     title: "GoWedding ",
     des: "an event management and wedding organizer system built using CodeIgniter 4 (CI4). various application features, such as managing event details, client information, budgets, and schedules, designed to simplify the planning and organization of events such as weddings..",
     img: "/Gowedding.png",
-    iconLists: ["/ci4.svg", "/boostrap.jpeg"],
+    iconLists: ["/ci4.svg", "/boostrap.jpeg", "/mysql.jpeg"],
     link: "https://github.com/dawijaya/ci4-gowedding",
   },
 
-  {
-    id: 4,
-    title: "Coming Soon",
-    des: "Stay tuned for more of my work",
-    img: "/coming.png",
-    iconLists: [
-      "/node.png",
-      "/ci4.svg",
-      "/express.png",
-      "/laravel.png",
-      "/api.png",
-    ],
-    link: "#",
-  },
+  // {
+  //   id: 4,
+  //   title: "Coming Soon",
+  //   des: "Stay tuned for more of my work",
+  //   img: "/coming.png",
+  //   iconLists: [
+  //     "/node.png",
+  //     "/ci4.svg",
+  //     "/express.png",
+  //     "/laravel.png",
+  //     "/api.png",
+  //     "/mongodb.jpeg",
+  //     "/mysql.jpeg",
+  //   ],
+  //   link: "#",
+  // },
 ];
